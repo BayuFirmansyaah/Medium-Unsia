f60f49b42c18eb20f7b399481e5a591ba0f9c244
BayuFirmansyaah <bayufirmansyah2800@gmail.com>
2024-07-26 06:46:56 +0700

    (init) - project laravel

---
+++ database/migrations/2024_07_25_233051_create_contents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id')->constrained('users')->onDelete('cascade');
+            $table->string('title');
+            $table->text('content');
+            $table->string('slug')->unique();
+            $table->string('status')->default('draft');
+            $table->string('type')->default('post');
+            $table->string('comment_status')->default('open');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents');
+    }
+};
